04e4320d321022893ff2711fb3c63f379ff378d5
Sergio <sergio.martins@points.com>
2020-10-05 09:13:32 -0400

    Set perf for 8 CPUs

--- cpu-gov-performance.sh
+++ cpu-gov-performance.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 
+
+sudo cpufreq-set -r -c 0 -g performance
+sudo cpufreq-set -r -c 1 -g performance
+sudo cpufreq-set -r -c 2 -g performance
+sudo cpufreq-set -r -c 3 -g performance
+sudo cpufreq-set -r -c 4 -g performance
+sudo cpufreq-set -r -c 5 -g performance
+sudo cpufreq-set -r -c 6 -g performance
+sudo cpufreq-set -r -c 7 -g performance
+
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 # cpufreq-set -c 0 -g performance
